c516aa3df96899736eef2d3fba924fd6614d1090
stonemoney <nickyg515@yahoo.com>
2017-06-21 10:37:00 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,137 @@
+console.log("Running MICROSB [@ngiano]! Close this window or press Ctrl+C when you are finished");
+//node-modules
+const WebSocket = require('ws'),
+	path = require("path"),
+	fs = require('fs-extra'),
+	opn = require("opn"),
+	glob = require("glob")
+const wss = new WebSocket.Server({ port: 8081 });
+//paths for required files
+var websocketDLL = path.join(__dirname, '/websocket.dll')
+var scriptExe = path.join(__dirname, '/MicroSB AutoHotKey Script.exe')
+var sbImgE = path.join(__dirname, '/sbE.png')
+var sbImg1 = path.join(__dirname, '/sb1.png')
+var sbImg2 = path.join(__dirname, '/sb2.png')
+var sbImg3 = path.join(__dirname, '/sb3.png')
+var sbImg4 = path.join(__dirname, '/sb4.png')
+var sbImg5 = path.join(__dirname, '/sb5.png')
+var sbImg6 = path.join(__dirname, '/sb6.png')
+var sbImg7 = path.join(__dirname, '/sb7.png')
+var sbImg8 = path.join(__dirname, '/sb8.png')
+var sbImg9 = path.join(__dirname, '/sb9.png')
+var sbImg10 = path.join(__dirname, '/sb10.png')
+var sbSnd1 = path.join(__dirname, '/sb1.mp3')
+var sbSnd2 = path.join(__dirname, '/sb2.mp3')
+var sbSnd3 = path.join(__dirname, '/sb3.mp3')
+var sbSnd4 = path.join(__dirname, '/sb4.mp3')
+var sbSnd5 = path.join(__dirname, '/sb5.mp3')
+var sbSnd6 = path.join(__dirname, '/sb6.mp3')
+var sbSnd7 = path.join(__dirname, '/sb7.mp3')
+var sbSnd8 = path.join(__dirname, '/sb8.mp3')
+var sbSnd9 = path.join(__dirname, '/sb9.mp3')
+var sbSnd10 = path.join(__dirname, '/sb10.mp3')
+var result = path.join(__dirname, '/result.html')
+//app workspace
+const baseFolder = process.env.APPDATA+'\\MicroSoundboard';
+console.log("FILE LOCATIONS: "+baseFolder);
+const dataFolder = process.env.APPDATA+'\\MicroSoundboard\\data';
+//create folders for workspace, if they don't exist already
+if (!fs.existsSync(baseFolder)){
+    fs.mkdirSync(baseFolder);
+}
+if (!fs.existsSync(dataFolder)){
+    fs.mkdirSync(dataFolder);
+}
+if (!fs.existsSync(baseFolder+'\\bin')){
+    fs.mkdirSync(baseFolder+'\\bin');
+}
+if (!fs.existsSync(baseFolder+'\\ahk')){
+    fs.mkdirSync(baseFolder+'\\ahk');
+}
+if (!fs.existsSync(baseFolder+'\\ahk\\exe')){
+    fs.mkdirSync(baseFolder+'\\ahk\\exe');
+}
+if (!fs.existsSync(dataFolder+'\\sb1.png')){
+	fs.copySync(sbImg1,dataFolder+'\\sb1.png');
+}
+if (!fs.existsSync(dataFolder+'\\sb2.png')){
+	fs.copySync(sbImg2,dataFolder+'\\sb2.png');
+}
+if (!fs.existsSync(dataFolder+'\\sb3.png')){
+	fs.copySync(sbImg3,dataFolder+'\\sb3.png');
+}
+if (!fs.existsSync(dataFolder+'\\sb4.png')){
+	fs.copySync(sbImg4,dataFolder+'\\sb4.png');
+}
+if (!fs.existsSync(dataFolder+'\\sb5.png')){
+	fs.copySync(sbImg5,dataFolder+'\\sb5.png');
+}
+if (!fs.existsSync(dataFolder+'\\sb6.png')){
+	fs.copySync(sbImg6,dataFolder+'\\sb6.png');
+}
+if (!fs.existsSync(dataFolder+'\\sb7.png')){
+	fs.copySync(sbImg7,dataFolder+'\\sb7.png');
+}
+if (!fs.existsSync(dataFolder+'\\sb8.png')){
+	fs.copySync(sbImg8,dataFolder+'\\sb8.png');
+}
+if (!fs.existsSync(dataFolder+'\\sb9.png')){
+	fs.copySync(sbImg9,dataFolder+'\\sb9.png');
+}
+if (!fs.existsSync(dataFolder+'\\sb10.png')){
+	fs.copySync(sbImg10,dataFolder+'\\sb10.png');
+}
+if (!fs.existsSync(dataFolder+'\\sb1.mp3')){
+	fs.copySync(sbSnd1,dataFolder+'\\sb1.mp3');
+}
+if (!fs.existsSync(dataFolder+'\\sb2.mp3')){
+	fs.copySync(sbSnd2,dataFolder+'\\sb2.mp3');
+}
+if (!fs.existsSync(dataFolder+'\\sb3.mp3')){
+	fs.copySync(sbSnd3,dataFolder+'\\sb3.mp3');
+}
+if (!fs.existsSync(dataFolder+'\\sb4.mp3')){
+	fs.copySync(sbSnd4,dataFolder+'\\sb4.mp3');
+}
+if (!fs.existsSync(dataFolder+'\\sb5.mp3')){
+	fs.copySync(sbSnd5,dataFolder+'\\sb5.mp3');
+}
+if (!fs.existsSync(dataFolder+'\\sb6.mp3')){
+	fs.copySync(sbSnd6,dataFolder+'\\sb6.mp3');
+}
+if (!fs.existsSync(dataFolder+'\\sb7.mp3')){
+	fs.copySync(sbSnd7,dataFolder+'\\sb7.mp3');
+}
+if (!fs.existsSync(dataFolder+'\\sb8.mp3')){
+	fs.copySync(sbSnd8,dataFolder+'\\sb8.mp3');
+}
+if (!fs.existsSync(dataFolder+'\\sb9.mp3')){
+	fs.copySync(sbSnd9,dataFolder+'\\sb9.mp3');
+}
+if (!fs.existsSync(dataFolder+'\\sb10.mp3')){
+	fs.copySync(sbSnd10,dataFolder+'\\sb10.mp3');
+}
+//regex
+var pattern1 = "^sb ([0-9]+)";
+var re1 = new RegExp(pattern1,'i');
+//send local copies of files to workspace
+fs.copySync(result,baseFolder+'/result.html');
+fs.copySync(sbImgE,dataFolder+'/sbE.png');
+fs.copySync(websocketDLL,baseFolder+'/bin/websocket.dll');
+fs.copySync(scriptExe,baseFolder+'/ahk/exe/MicroSB AutoHotKey Script.exe')
+//open autohotkey file
+opn(baseFolder+'/ahk/exe/MicroSB AutoHotKey Script.exe');
+wss.on('connection', function connection(ws) {
+	ws.on('message', function incoming(message) {
+		if(re1.test(message)) {
+			var matches = message.match(pattern1);
+			wss.clients.forEach(function each(client) {
+				if (client !== ws && client.readyState === WebSocket.OPEN) {
+					client.send("trigger "+matches[1]);
+				}
+			});
+		}
+
+	});
+
+});
---
+++ microsoundboard.ahk
@@ -0,0 +1,70 @@
+﻿;msb@ngiano
+#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
+; #Warn  ; Enable warnings to assist with detecting common errors.
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+#SingleInstance Force
+#include ..\..\include\ahk\websocket.ahk
+OnExit("Disconnect")
+websocket_registerCallback(1, "on_fail")
+websocket_registerCallback(2, "on_disconnect")
+Sleep, 1000
+websocket_connect("ws://localhost:8081")
+return
+on_fail()
+{
+	Critical
+	MsgBox, 5, Server client connection was not established, Would you like to retry or abort connection?
+	IfMsgBox Retry
+		Reload
+		return
+    IfMsgBox Cancel
+		Exit
+		return
+}
+on_disconnect()
+{
+	Critical
+	MsgBox, 5, Server client connection was lost, Would you like to retry or abort connection?
+	IfMsgBox Retry
+		Reload
+		return
+    IfMsgBox Cancel
+		Exit
+		return
+}
+f13::
+websocket_send("sb 1", strLen("sb 1"), false)
+return
+f14::
+websocket_send("sb 2", strLen("sb 2"), false)
+return
+f15::
+websocket_send("sb 3", strLen("sb 3"), false)
+return
+f16::
+websocket_send("sb 4", strLen("sb 4"), false)
+return
+f17::
+websocket_send("sb 5", strLen("sb 5"), false)
+return
+f18::
+websocket_send("sb 6", strLen("sb 6"), false)
+return
+f19::
+websocket_send("sb 7", strLen("sb 7"), false)
+return
+f20::
+websocket_send("sb 8", strLen("sb 8"), false)
+return
+f21::
+websocket_send("sb 9", strLen("sb 9"), false)
+return
+f22::
+websocket_send("sb 10", strLen("sb 10"), false)
+return
+
+Disconnect() 
+{
+websocket_disconnect()
+return
+}
